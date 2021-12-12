6e8c4d7a01ff706a0b0b9f955433538335c3cced
Ascetic313 <iddrisualmuqadas74@gmail.com>
2021-12-12 01:12:44 +0000

    This is a project submission on ASCII Project

---
+++ ASCII_Project.sh
@@ -0,0 +1,16 @@
+$ # Adding files to staging area:
+$ git add portrait.txt
+$ git commit -m "This is a project submission on Portrait"
+On branch master
+Untracked files:
+        house.txt
+
+$ # Reseting git to remove house.txt:
+$ git rm house.txt
+
+
+$ git commit -m "This is a change"
+On branch master
+Untracked files:
+        house.txt
+ 
